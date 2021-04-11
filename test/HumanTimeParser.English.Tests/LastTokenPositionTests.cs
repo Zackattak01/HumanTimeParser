05f69651a764cb184bb20b55d038f75254d470d3
Zack Broderson <zbroderson@hotmail.com>
2021-04-10 21:13:59 -0400

    Added More Test Coverage

---
+++ test/HumanTimeParser.English.Tests/LastTokenPositionTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using HumanTimeParser.Core.Parsing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HumanTimeParser.English.Tests
+{
+    [TestClass]
+    public class LastTokenPositionTests
+    {
+        [TestMethod]
+        public void LastTokenPositionTest()
+        {
+            var result = (ISuccessfulTimeParsingResult<DateTime>)EnglishTimeParser.Parse("in 5 s do things cool stuff");
+
+            Assert.AreEqual(2, result.LastParsedTokenPosition);
+        }
+
+        [TestMethod]
+        public void LastTokenPositionTest_Alt()
+        {
+            var result = (ISuccessfulTimeParsingResult<DateTime>)EnglishTimeParser.Parse("10s gamer time");
+
+            Assert.AreEqual(0, result.LastParsedTokenPosition);
+        }
+
+        [TestMethod]
+        public void LastTokenPositionTest_Alt2()
+        {
+            var result = (ISuccessfulTimeParsingResult<DateTime>)EnglishTimeParser.Parse("5:30pm files");
+
+            Assert.AreEqual(0, result.LastParsedTokenPosition);
+        }
+
+        [TestMethod]
+        public void LastTokenPositionTest_Alt3()
+        {
+            var result = (ISuccessfulTimeParsingResult<DateTime>)EnglishTimeParser.Parse("5:30pm .files");
+
+            Assert.AreEqual(0, result.LastParsedTokenPosition);
+        }
+
+        [TestMethod]
+        public void Spaced_AM_PM_Last_Token_Pos()
+        {
+            var result = (ISuccessfulTimeParsingResult<DateTime>)EnglishTimeParser.Parse("5:30 PM");
+
+            Assert.AreEqual(1, result.LastParsedTokenPosition);
+        }
+    }
+}
