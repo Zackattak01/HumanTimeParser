6760316ae23156a26b773c12924ce465f3f2b142
Zack Broderson <zbroderson@hotmail.com>
2021-05-12 17:10:03 -0400

    [ci skip] xml doc new extension methods

--- src/HumanTimeParser.Core/Extensions/DictionaryExtensions.cs
+++ src/HumanTimeParser.Core/Extensions/DictionaryExtensions.cs
@@ -2,8 +2,19 @@ using System.Collections.Generic;
 
 namespace HumanTimeParser.Core.Extensions
 {
+    /// <summary>
+    /// Useful dictionary extensions
+    /// </summary>
     public static class DictionaryExtensions
     {
+        /// <summary>
+        /// Adds all the specified key with the specified value.
+        /// </summary>
+        /// <param name="dict">The dictionary to perform the operation on.</param>
+        /// <param name="collection">The collection of keys</param>
+        /// <param name="value">The value for all the key</param>
+        /// <typeparam name="TKey">The type of the key</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
         public static void AddKeyRange<TKey, TValue>(this IDictionary<TKey, TValue> dict, IEnumerable<TKey> collection, TValue value)
         {
             foreach (var item in collection)
